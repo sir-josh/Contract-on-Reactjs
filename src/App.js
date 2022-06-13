cd158b323a6924b5b040366ba9c3f34e9539837e
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-06-13 15:20:29 +0100

    Show more info about to pool to users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react'
-import './App.css';
-import lottery from './lottery';
-
+import React, { Component } from "react";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends Component {
-  state = { 
-    manager: ''
-  }
+  state = {
+    manager: "",
+    players: [],
+    balancePool: "",
+  };
 
-  async componentDidMount(){
+  async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balancePool = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({  manager });
+    this.setState({ manager, players, balancePool });
   }
 
   render() {
+    const { manager, players, balancePool } = this.state;
+  
     return (
       <div className="App">
         <h2>lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is managed by {manager}</p>
+        <p>
+          There are currently { players.length} people that have entered the pool,
+          competing to win {web3.utils.fromWei(balancePool, "ether")} amount of
+          ether.
+        </p>
       </div>
     );
   }
