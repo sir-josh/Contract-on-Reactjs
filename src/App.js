952d072ed9b8dd0ace02c7776cc785a72d15b38f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-06-16 22:01:07 +0100

    Refactor page to include last winner

--- src/App.js
+++ src/App.js
@@ -4,207 +4,239 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends Component {
-  state = {
-    manager: "",
-    currentUserIsManager: false,
-    players: [],
-    balancePool: "",
-    value: "",
-    message: {
-      body: "",
-      type: "",
-      icon: "",
-    },
-  };
+	state = {
+		manager: "",
+		currentUserIsManager: false,
+		players: [],
+		balancePool: "",
+		value: "",
+		message: {
+			body: "",
+			type: "",
+			icon: "",
+		},
+		previousWinner: "",
+	};
 
-  async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
-    const players = await lottery.methods.getPlayers().call();
-    const balancePool = await web3.eth.getBalance(lottery.options.address);
-    
-    const currentUser = await web3.eth.getAccounts();
-    const currentUserIsManager = manager === currentUser[0];
+	async componentDidMount() {
+		const manager = await lottery.methods.manager().call();
+		const players = await lottery.methods.getPlayers().call();
+		const previousWinner = await lottery.methods.lastWinner().call();
+		const balancePool = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager, players, balancePool, currentUserIsManager });
-  }
+		const currentUser = await web3.eth.getAccounts();
+		const currentUserIsManager = manager === currentUser[0];
 
-  onFormSubmit = async (event) => {
-    event.preventDefault();
+		this.setState({
+			manager,
+			players,
+			balancePool,
+			currentUserIsManager,
+			previousWinner,
+		});
+	}
 
-    try {
-      const accounts = await web3.eth.getAccounts();
+	onFormSubmit = async (event) => {
+		event.preventDefault();
 
-      this.setState({
-        message: {
-          body: "Waiting to approve transaction",
-          type: "info",
-          icon: "info circle icon",
-        },
-      });
+		try {
+			const accounts = await web3.eth.getAccounts();
 
-      await lottery.methods.enter().send({
-        from: accounts[0],
-        value: web3.utils.toWei(this.state.value, "ether"),
-      });
+			this.setState({
+				message: {
+					body: "Waiting to approve transaction",
+					type: "info",
+					icon: "info circle icon",
+				},
+			});
 
-      this.setState({
-        message: {
-          body: "Transaction success. You're in the lottery pool!",
-          type: "success",
-          icon: "check icon",
-        },
-      });
-    } catch (error) {
-      this.setState({
-        message: {
-          body: error.message,
-          type: "error",
-          icon: "x icon",
-        },
-      });
-    }
-  };
+			await lottery.methods.enter().send({
+				from: accounts[0],
+				value: web3.utils.toWei(this.state.value, "ether"),
+			});
 
-  pickWinner = async () => {
-    try {
-      const accounts = await web3.eth.getAccounts();
+			this.setState({
+				message: {
+					body: "Transaction success. You're in the lottery pool!",
+					type: "success",
+					icon: "check icon",
+				},
+			});
+		} catch (error) {
+			this.setState({
+				message: {
+					body: error.message,
+					type: "error",
+					icon: "x icon",
+				},
+			});
+		}
+	};
 
-      this.setState({
-        message: {
-          body: "Waiting to pick a winner...",
-          type: "info",
-          icon: "info circle icon",
-        },
-      });
+	pickWinner = async () => {
+		try {
+			const accounts = await web3.eth.getAccounts();
 
-      await lottery.methods.pickWinner().send({
-        from: accounts[0],
-      });
+			this.setState({
+				message: {
+					body: "Waiting to pick a winner...",
+					type: "info",
+					icon: "info circle icon",
+				},
+			});
 
-      this.setState({
-        message: {
-          body: "A winner has been picked!",
-          type: "success",
-          icon: "check icon",
-        },
-      });
-    } catch (error) {
-      this.setState({
-        message: {
-          body: error.message,
-          type: "error",
-          icon: "x icon",
-        },
-      });
-    }
-  };
+			await lottery.methods.pickWinner().send({
+				from: accounts[0],
+			});
 
-  showPickWinner = () => {
-      return(
-        <div>
-            <h4>Ready to pick a winner</h4>
-            <button
-              onClick={this.pickWinner}
-              className="ui secondary button"
-              style={{ marginTop: "1.3rem", padding: "15px 50px" }}>
-              Pick a winner!
-            </button>
-        </div>
-      )
-  }
+			this.setState({
+				message: {
+					body: "A winner has been picked!",
+					type: "success",
+					icon: "check icon",
+				},
+			});
+		} catch (error) {
+			this.setState({
+				message: {
+					body: error.message,
+					type: "error",
+					icon: "x icon",
+				},
+			});
+		}
+	};
 
-  hidePickWinner = () =>{
-    return (
-      <div class="ui info message">
-        <i class="info icon"></i>
-        The manager will be the one to shuffle the pool. Winner will automatically be credited.
-      </div>
-    )
-  }
+	showPickWinner = () => {
+		return (
+			<div>
+				<h4>Ready to pick a winner</h4>
+				<button
+					onClick={this.pickWinner}
+					className="ui secondary button"
+					style={{ marginTop: "1.3rem", padding: "15px 50px" }}>
+					Pick a winner!
+				</button>
+			</div>
+		);
+	};
 
-  render() {
-    const { manager, players, balancePool, value, message } = this.state;
+	hidePickWinner = () => {
+		return (
+			<div className="ui info message">
+				<i className="info icon"></i>
+				The manager will be the one to shuffle the pool. The lucky winner will
+				automatically be credited.
+			</div>
+		);
+	};
 
-    return (
-      <div style={{ paddingBottom: "3rem"}}>
-        <h2 className="ui inverted header vertical center aligned segment">
-          LOTTERY CONTRACT
-        </h2>
+	render() {
+		const { manager, players, balancePool, value, message, previousWinner } = this.state;
 
-        <div className="ui fluid container">
-          <div className="ui block header very padded text center aligned segment">
-            <p className="ui tiny header">
-              This contract is managed by {manager}
-            </p>
-            <p className="sub header">
-              There are currently <b>{players.length}</b> people that have
-              entered the pool, competing to win{" "}
-              <b>{web3.utils.fromWei(balancePool, "ether")}</b> amount of ether.
-            </p>
-          </div>
-        </div>
+		return (
+			<div style={{ paddingBottom: "3rem" }}>
+				<h2 className="ui inverted header vertical center aligned segment">
+					LOTTERY CONTRACT
+				</h2>
 
-        {/* Pick Winner by the manager start here */}
-        <div className="ui container" style={{ marginTop: "3rem"}}>
-          {this.state.currentUserIsManager&&
-            <div className="ui fluid centered card" style={{ marginBottom:"2rem", padding: "2rem" }}>
-              {this.showPickWinner()}
-            </div>
-          }
-          {!this.state.currentUserIsManager&&
-            this.hidePickWinner()
-          }
-        </div>
-        {/* Pick Winner by the manager ends here */}
-
-        <div className="ui container" style={{ marginTop: "2rem" }}>
-          <div className="ui fluid centered card" style={{ padding: "2rem" }}>
-            <div className="content">
-              <form onSubmit={this.onFormSubmit} className="ui fluid form">
-                <h3 className="ui block header">Want to try your luck?</h3>
-
-                <div className="eight wide field centered" style={{ marginTop: "3rem" }}>
-                  <div className="ui pointing below label">
-                    Amount of ether to enter
-                  </div>
-
-                  <div className="ui right labeled input">
-                    <input
-                      value={value}
-                      onChange={(e) => this.setState({ value: e.target.value })}
-                      type="number"
-                      placeholder="0.2"
-                      required
-                    />
-                    <div className="ui basic label">Ether</div>
-                  </div>
-                </div>
-
-                <button
-                  className="ui secondary button"
-                  style={{ marginTop: "1.3rem", padding: "15px 50px" }}>
-                  Enter
-                </button>
-              </form>
-            </div>
-    
-            {/* Message body start here */}
-            {message.body && (
-              <div className={`ui ${message.type} message`}>
-                <i className={`large ${message.icon}`}></i>
-                {message.body} &nbsp;&nbsp;
-                {message.type === "info" && (
-                  <i className="large spinner loading icon"></i>
-                )}
+				<div className="ui fluid container">
+					<div className="ui block header very padded text center aligned segment">
+						<p className="ui tiny header">
+							This lottery is managed by {manager}
+						</p>
+						<p className="sub header">
+							There are currently <b>{players.length}</b> people
+							that have entered the pool, competing to win{" "}
+							<b>{web3.utils.fromWei(balancePool, "ether")}</b>{" "}
+							amount of ether.
+						</p>            
+            <div className="ui transparent basic secondary segment">
+              <div className="ui black label">
+                <b>Previous winner:</b>
+                <div className="detail">{previousWinner}</div>
               </div>
-            )}
-            {/* Message body end here */}
-     
-          </div>
-        </div>
-      </div>
-    );
-  }
+            </div>
+					</div>
+				</div>
+
+				{/* Pick Winner by the manager start here */}
+				<div className="ui container" style={{ marginTop: "3rem" }}>
+					{this.state.currentUserIsManager && (
+						<div
+							className="ui fluid centered card"
+							style={{ marginBottom: "2rem", padding: "2rem" }}>
+							{this.showPickWinner()}
+						</div>
+					)}
+					{!this.state.currentUserIsManager && this.hidePickWinner()}
+				</div>
+				{/* Pick Winner by the manager ends here */}
+
+				<div className="ui container" style={{ marginTop: "2rem" }}>
+					<div
+						className="ui fluid centered card"
+						style={{ padding: "2rem" }}>
+						<div className="content">
+							<form
+								onSubmit={this.onFormSubmit}
+								className="ui fluid form">
+								<h3 className="ui block header">
+									Want to try your luck?
+								</h3>
+
+								<div
+									className="eight wide field centered"
+									style={{ marginTop: "3rem" }}>
+									<div className="ui pointing below label">
+										Amount of ether to enter
+									</div>
+
+									<div className="ui right labeled input">
+										<input
+											value={value}
+											onChange={(e) =>
+												this.setState({
+													value: e.target.value,
+												})
+											}
+											type="number"
+											placeholder="0.2"
+											required
+										/>
+										<div className="ui basic label">
+											Ether
+										</div>
+									</div>
+								</div>
+
+								<button
+									className="ui secondary button"
+									style={{
+										marginTop: "1.3rem",
+										padding: "15px 50px",
+									}}>
+									Enter
+								</button>
+							</form>
+						</div>
+
+						{/* Message body start here */}
+						{message.body && (
+							<div className={`ui ${message.type} message`}>
+								<i className={`large ${message.icon}`}></i>
+								{message.body} &nbsp;&nbsp;
+								{message.type === "info" && (
+									<i className="large spinner loading icon"></i>
+								)}
+							</div>
+						)}
+						{/* Message body end here */}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
